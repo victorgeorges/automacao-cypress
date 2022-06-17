b07c19c61c5b6735dbbdc373d8671bde7bfdf36d
victor.george <victor.george@valtech.com>
2022-06-17 09:07:25 -0300

    first forking

--- cypress/integration/browser-testing/sample1.spec.js
+++ cypress/integration/browser-testing/sample1.spec.js
@@ -4,8 +4,10 @@ describe('Browser testing bad practice - Sample 1', () => {
   })
 
   it('directs the user to the login page when clicking the login link', () => {
-    cy.contains('.nav a', 'Login').click()
-
-    cy.url().should('be.equal', 'https://notes-serverless-app.com/login')
+    //uma possibilidade =>     cy.contains('.nav a', 'Login').click().should('have.attr', 'href', '/login')
+    //outra possibilidade 
+    //(aqui tu ta falando pra ele verifica que não há o atributo de target que é a mudança de página)
+    cy.contains('.nav a', 'Login').should('have.attr', 'href', '/login')
+    .and('not.have.attr' , 'target')
   })
 })
